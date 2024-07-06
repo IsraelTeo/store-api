a99bf25ff7b7fac1178b9d3aa410bf015ea8e89b
Israel <israel.bastion@gmail.com>
2024-07-05 21:22:39 -0500

    Atributtes of model: customer, user added. Missing relations.

--- src/main/java/com/store/api/persistence/model/Customer.java
+++ src/main/java/com/store/api/persistence/model/Customer.java
@@ -1,4 +1,30 @@
 package com.store.api.persistence.model;
 
-public class Customer {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "customers")
+public class Customer extends User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    private String dni;
+
+    //private Sale sale;
+
 }
--- src/main/java/com/store/api/persistence/model/User.java
+++ src/main/java/com/store/api/persistence/model/User.java
@@ -1,4 +1,32 @@
 package com.store.api.persistence.model;
 
+import com.store.api.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+@Table(name = "customers")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String email;
+
+    private String userName;
+
+    private String password;
+
+    //private List<Role> roles;
+
+
 }
